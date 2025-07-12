8bf020fd1509ffdf86c46e96e643a78446044647
StepanBandera-droid <ribinstepan10@mail.ru>
2025-07-12 15:06:07 +0300

    Adding a star to the card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -33,6 +33,19 @@ const Card = ({ image, onToggleFavorite }: ICard): ReactElement => {
       >
         {image.isFavorite ? "Сохранено" : "Сохранить"}
       </button>
+      <svg
+        xmlns="https://i.pinimg.com/originals/8f/d4/5a/8fd45a77cea7467eea1d3ac6fe575ba2.png"
+        viewBox="0 0 24 24"
+        fill="gold"
+        width="24"
+        height="24"
+        style={{
+          opacity: image.isFavorite ? 1 : 0,
+          visibility: isHovered ? "visible" : "hidden",
+        }}
+      >
+        <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+      </svg>
     </div>
   );
 };
